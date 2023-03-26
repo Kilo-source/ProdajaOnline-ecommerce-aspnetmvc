2b0f4894524b7bfb0830be64b3c03154323acca9
Kilo-source <ludikilo02@gmail.com>
2023-03-27 00:35:58 +0200

    Add project files.

---
+++ ProdajaOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProdajaOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
